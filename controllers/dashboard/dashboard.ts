4addce5d84dc20751c61af60be9a390569653e47
Dylan Weijgertze <dylanwprivate@gmail.com>
2022-07-12 23:51:34 +0200

    added getWorkoutHistory

--- controllers/dashboard/dashboard.ts
+++ controllers/dashboard/dashboard.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import { getAllTemplatesForUser } from '../../models/templates'
-import db from '../../db/connection';
+import { getWorkoutHistory } from '../../models/workouts'
 
 const router = express.Router();
 
@@ -8,18 +8,8 @@ router.get('/', async (req: Request | any, res: Response) => {
 	const { user } = req;
 	
 	const templates = await getAllTemplatesForUser(user.id);
-
-	// get the 5 most recent workouts
-	const [workouts] = await db.query(
-		`
-		SELECT W.name, DATE_FORMAT(W.date, "%d-%m-%Y") AS date, HOUR(W.time) as time_hour, MINUTE(W.time) as time_minute
-		FROM workout AS W
-		WHERE user_id = ?
-		ORDER BY W.id DESC
-		LIMIT 5;
-		`,
-		[user.id]
-	);
+	
+	const workouts = await getWorkoutHistory(5, user.id);
 
 	res.render('dashboard/dashboard', { user, templates, workouts });
 });
